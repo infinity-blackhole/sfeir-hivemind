aa137e7d97d383768d1f61bb86d1c7638a7667c8
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-06-05 10:52:44 +0000

    Cleanup boilerplate

--- python_template_project/app.py
+++
@@ -1,6 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present William Phetsinorath
-#
-# SPDX-License-Identifier: MIT
-
-if __name__ == "__main__":
-    print("Hello World!")
--- python_template_project/lib_test.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present William Phetsinorath
-#
-# SPDX-License-Identifier: MIT
-
-from . import sum
-
-
-def test_sum():
-    assert sum(1, 2) == 3
