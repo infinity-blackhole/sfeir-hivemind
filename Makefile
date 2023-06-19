1fb77318683c8e1ef352763c296d15e408296664
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-06-19 19:38:42 +0000

    Refactor using BentoML

--- Makefile
+++ Makefile
@@ -2,20 +2,7 @@
 all: sync gen containers
 
 gen:
-	@python -m grpc_tools.protoc \
-		-I . \
-		-I third_party/googleapis \
-		--include_imports \
-		--include_source_info \
-		--descriptor_set_out=sfeir/hivemind/protos/proto.pb \
-		--pyi_out=. \
-		--python_out=. \
-		--grpc_python_out=. \
-		sfeir/hivemind/protos/*.proto
-
-.PHONY: sync
-sync:
-	@hatch dep show requirements --project-only > requirements.txt
+	@bentoml build
 
 .PHONY: containers
 containers:
---
+++ sfeir/hivemind/service.py
@@ -0,0 +1,40 @@
+import os
+
+import bentoml
+import vertexai
+from bentoml.io import Text
+from langchain.chains import ConversationalRetrievalChain
+from langchain.embeddings import VertexAIEmbeddings
+from langchain.llms import VertexAI
+from langchain.vectorstores import DeepLake
+
+llm = VertexAI()
+embeddings = VertexAIEmbeddings()
+
+vectorstore = DeepLake(
+    dataset_path="gcs://shikanime-studio-labs-hivemind-deep-lake-dataset/books/",
+    embedding_function=embeddings,
+    read_only=True,
+)
+
+qa = ConversationalRetrievalChain.from_llm(
+    llm,
+    vectorstore.as_retriever(),
+    return_source_documents=True,
+    verbose=True,
+)
+
+svc = bentoml.Service("sfeir_hivemind")
+
+
+@svc.on_startup
+async def vertexai_init_on_startup(context: bentoml.Context):
+    project = os.environ.get("GOOGLE_CLOUD_PROJECT", None)
+    location = os.environ.get("GOOGLE_CLOUD_LOCATION", None)
+    vertexai.init(project=project, location=location)
+
+
+@svc.api(input=Text(), output=Text())
+async def classify(question: str) -> str:
+    result = qa({"question": question, "chat_history": []})
+    return result["answer"]
