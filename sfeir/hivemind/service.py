9b1aa35b3bd89e545d6230845559fd7025e21243
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-06-20 10:11:09 +0000

    Use internal Vertex AI init

--- sfeir/hivemind/service.py
+++ sfeir/hivemind/service.py
@@ -2,7 +2,6 @@ import os
 from typing import cast
 
 import bentoml
-import vertexai
 from bentoml.io import Text
 from langchain.chains import ConversationalRetrievalChain
 from langchain.embeddings import VertexAIEmbeddings
@@ -16,16 +15,20 @@ svc = bentoml.Service("sfeir-hivemind")
 async def on_startup(context: bentoml.Context):
     project = os.environ.get("GOOGLE_CLOUD_PROJECT")
     location = os.environ.get("GOOGLE_CLOUD_LOCATION")
+    if location != "us-central1":
+        raise bentoml.exceptions.InvalidArgument(
+            "Only us-central1 location is supported"
+        )
     dataset_path = os.environ.get("DEEP_LAKE_DATASET_URI")
     if dataset_path is None:
         raise bentoml.exceptions.InvalidArgument(
             "DEEP_LAKE_DATASET_URI environment variable not set"
         )
 
-    vertexai.init(project=project, location=location)
-
-    llm = context.state["llm"] = VertexAI()
-    embeddings = context.state["embeddings"] = VertexAIEmbeddings()
+    llm = context.state["llm"] = VertexAI(project=project, location=location)
+    embeddings = context.state["embeddings"] = VertexAIEmbeddings(
+        project=project, location=location
+    )
     vectorstore = context.state["vectorstore"] = DeepLake(
         dataset_path=dataset_path,
         embedding_function=embeddings,
