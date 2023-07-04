1f919ba4c15d97cd140784dab45076e3f8c2605a
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-07-04 14:56:50 +0000

    Lower model size

--- sfeir/hivemind/models/llm.py
+++ sfeir/hivemind/models/llm.py
@@ -3,6 +3,6 @@ from transformers import AutoModelForCausalLM
 
 bentoml.transformers.save_model(
     "sfeir_hivemind_llm",
-    AutoModelForCausalLM.from_pretrained("StabilityAI/stablelm-tuned-alpha-7b"),
+    AutoModelForCausalLM.from_pretrained("StabilityAI/stablelm-tuned-alpha-3b"),
     signatures={"generate": {"batchable": False, "batch_dim": 0}},
 )
