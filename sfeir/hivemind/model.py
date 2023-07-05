a300747f592412a0505608c73a88be8a18f6412a
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-07-05 14:25:50 +0000

    Apply quantization

--- sfeir/hivemind/model.py
+++
@@ -1,6 +0,0 @@
-import bentoml
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
-
-bentoml.pytorch.save_model("sentence-transformers", model)
--- sfeir/hivemind/models/llm.py
+++ sfeir/hivemind/models/llm.py
@@ -4,14 +4,19 @@ import transformers
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("tiiuae/falcon-7b", trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-7b", trust_remote_code=True)
+
+model = AutoModelForCausalLM.from_pretrained(
+    "tiiuae/falcon-7b",
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    load_in_8bit=True,
+    device_map="auto",
+)
+
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
     tokenizer=tokenizer,
-    torch_dtype=torch.bfloat16,
-    trust_remote_code=True,
-    device_map="auto",
 )
 
 bentoml.transformers.save_model("sfeir-hivemind-falcon-7b", pipeline)
