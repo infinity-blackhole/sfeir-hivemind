8fd41a7afa06610cc043544d7eafe3225ce95f7f
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-07-05 16:31:57 +0000

    Quantization Falcon

--- sfeir/hivemind/models/llm.py
+++ sfeir/hivemind/models/llm.py
@@ -1,16 +1,19 @@
 import bentoml
 import torch
 import transformers
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
 tokenizer = AutoTokenizer.from_pretrained("tiiuae/falcon-7b", trust_remote_code=True)
 
 model = AutoModelForCausalLM.from_pretrained(
     "tiiuae/falcon-7b",
-    torch_dtype=torch.bfloat16,
     trust_remote_code=True,
+    torch_dtype=torch.bfloat16,
     load_in_8bit=True,
     device_map="auto",
+    quantization_config=BitsAndBytesConfig(
+        llm_int8_enable_fp32_cpu_offload=True,
+    ),
 )
 
 pipeline = transformers.pipeline(
@@ -19,4 +22,4 @@ pipeline = transformers.pipeline(
     tokenizer=tokenizer,
 )
 
-bentoml.transformers.save_model("sfeir-hivemind-falcon-7b", pipeline)
+bentoml.transformers.save_model("sfeir-hivemind-falcon-7b", pipeline=pipeline)
