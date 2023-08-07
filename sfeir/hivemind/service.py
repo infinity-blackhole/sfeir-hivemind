7e06dbe97dfb8a5e3b5dddc6c9f34511cf6a1e8f
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-08-07 15:59:46 +0000

    Revert langchain components to global

--- sfeir/hivemind/service.py
+++ sfeir/hivemind/service.py
@@ -15,8 +15,22 @@ from sfeir.hivemind.schema import (
 from sfeir.langchain.embeddings.hivemind import HivemindEmbeddings
 
 embeddings = HivemindEmbeddings(model_name="pt-sentence-transformers-all-mpnet-base-v2")
-
 svc = bentoml.Service("sfeir_hivemind", runners=[embeddings.runner])
+
+vectorstore = DeepLake(
+    dataset_path=os.environ["DEEP_LAKE_DATASET_URI"],
+    embedding_function=embeddings,
+    read_only=True,
+)
+llm = VertexAI(
+    project=os.environ["VERTEX_AI_PROJECT"],
+    location=os.environ["VERTEX_AI_LOCATION"],
+)
+chain = ConversationalRetrievalChain.from_llm(
+    llm,
+    vectorstore.as_retriever(),
+    return_source_documents=True,
+)
 
 
 @svc.api(
@@ -28,20 +42,6 @@ async def predict(qa: QuestionAnsweringRequest) -> QuestionAnsweringResponse:
         collection_name="chat_history",
         session_id=qa.session_id,
         user_id=qa.user_id,
-    )
-    vectorstore = DeepLake(
-        dataset_path=os.environ["DEEP_LAKE_DATASET_URI"],
-        embedding_function=embeddings,
-        read_only=True,
-    )
-    llm = VertexAI(
-        project=os.environ["VERTEX_AI_PROJECT"],
-        location=os.environ["VERTEX_AI_LOCATION"],
-    )
-    chain = ConversationalRetrievalChain.from_llm(
-        llm,
-        vectorstore.as_retriever(),
-        return_source_documents=True,
     )
     result = chain(
         {"question": qa.question, "chat_history": chat_message_history.messages}
