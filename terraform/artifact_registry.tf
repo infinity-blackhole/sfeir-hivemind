0267a2e2e62eb01f1d58079f0b86dc31774590b8
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-08-07 12:48:08 +0000

    Upgrade Terraform

--- terraform/artifact_registry.tf
+++ terraform/artifact_registry.tf
@@ -1,10 +1,14 @@
 module "docker_artifact_registry" {
   source     = "github.com/GoogleCloudPlatform/cloud-foundation-fabric//modules/artifact-registry"
   project_id = var.project_id
+  name       = var.name
   location   = var.location
   labels     = var.labels
-  format     = "DOCKER"
-  id         = var.name
+  format = {
+    docker = {
+      immutable_tags = true
+    }
+  }
   iam = {
     "roles/artifactregistry.reader" = [module.service_account.iam_email]
   }
